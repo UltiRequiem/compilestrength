38143ddf38831e57974164ec43097d22003fcd21
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-11-08 10:00:47 -0500

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 import { env } from "../env";
 
 export const authClient = createAuthClient({
-	baseURL: env.BETTER_AUTH_URL|| "http://localhost:3000",
+	baseURL: env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
